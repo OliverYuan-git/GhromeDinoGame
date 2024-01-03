c85e92ca14e7bca2c579751ab12ffa71c91b4e07
Oliver Yuan <125700066+OliverYuan-git@users.noreply.github.com>
2024-01-03 16:42:42 -0500

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private CharacterController character;
+    private Vector3 direction;
+
+    public float gravity = 10f * 2f;
+    public float jumpForce = 10f;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        direction += Vector3.down * gravity * Time.deltaTime;
+
+        if (character.isGrounded)
+        {
+           direction = Vector3.down;
+
+           if(Input.GetButton("Jump"))
+           {
+            direction = Vector3.up * jumpForce;
+           }
+        }
+
+        character.Move(direction * Time.deltaTime);
+
+        
+    }
+}
